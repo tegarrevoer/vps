2e68845afbaccebf9fa01a90abf82a8e308dfa7b
tegarrevoer <75308635+tegarrevoer@users.noreply.github.com>
2022-11-23 18:51:00 +0700

    Add files via upload

---
+++ about.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+clear
+echo -e "क══════════क⊹⊱✫⊰⊹क═══════════क" | lolcat
+echo -e "#         TEGAR INET TEAM                  #
+echo -e "#क══════════क⊹⊱✫⊰⊹क══════════क#"
+echo -e "# For Debian 9 & Debian 10 64 bit               #"
+echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #"
+echo -e "# For VPS with KVM and VMWare virtualization    #"
+echo -e "# Build Up By TEGAR INET TEAM                      #"
+echo -e "#क══════════क⊹⊱✫⊰⊹क══════════क#"
+echo -e "# Thanks To                                     #"
+echo -e "#क══════════क⊹⊱✫⊰⊹क══════════क#"
+echo -e "#                                               #"
+echo -e "# Allah                                         #"
+echo -e "# My Family                                     #"
+echo -e "#क══════════क⊹⊱✫⊰⊹क══════════क#"
+echo -e "क══════════क⊹⊱✫⊰⊹क═══════════क"
