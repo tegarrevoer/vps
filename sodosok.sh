021975e104d80cfc56fc6e41d043b400802eb30d
tegarrevoer <75308635+tegarrevoer@users.noreply.github.com>
2022-11-23 21:13:37 +0700

    Update sodosok.sh

--- sodosok.sh
+++ sodosok.sh
@@ -88,10 +88,10 @@ iptables -I INPUT -m state --state NEW -m udp -p udp --dport 2443:3543 -j ACCEPT
 iptables-save > /etc/iptables.up.rules
 ip6tables-save > /etc/ip6tables.up.rules
 cd /usr/bin
-wget -O add-ss "https://raw.githubusercontent.com/santzx45/scmzx/main/add-ss.sh"
-wget -O del-ss "https://raw.githubusercontent.com/santzx45/scmzx/main/del-ss.sh"
-wget -O cek-ss "https://raw.githubusercontent.com/santzx45/scmzx/main/cek-ss.sh"
-wget -O renew-ss "https://raw.githubusercontent.com/santzx45/scmzx/main/renew-ss.sh"
+wget -O add-ss "https://raw.githubusercontent.com/tegarrevoer/vps/main/add-ss.sh"
+wget -O del-ss "https://raw.githubusercontent.com/tegarrevoer/vps/main/del-ss.sh"
+wget -O cek-ss "https://raw.githubusercontent.com/tegarrevoer/vps/main/cek-ss.sh"
+wget -O renew-ss "https://raw.githubusercontent.com/tegarrevoer/vps/main/renew-ss.sh"
 chmod +x add-ss
 chmod +x del-ss
 chmod +x cek-ss
