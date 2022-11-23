a9beb50775f9bf77d59af3870a9e7df390e9e662
tegarrevoer <75308635+tegarrevoer@users.noreply.github.com>
2022-11-23 19:58:35 +0700

    Update vpn.sh

--- vpn.sh
+++ vpn.sh
@@ -15,7 +15,7 @@ apt install openvpn easy-rsa unzip -y
 apt install openssl iptables iptables-persistent -y
 mkdir -p /etc/openvpn/server/easy-rsa/
 cd /etc/openvpn/
-wget https://raw.githubusercontent.com/santzx45/scmzx/main/vpn.zip
+wget https://raw.githubusercontent.com/tegarrevoer/vps/main/vpn.zip
 unzip vpn.zip
 rm -f vpn.zip
 chown -R root:root /etc/openvpn/server/easy-rsa/
