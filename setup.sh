639820fe65b5175fd108b88714e339f622f25cdf
tegarrevoer <75308635+tegarrevoer@users.noreply.github.com>
2022-11-23 19:16:05 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -17,7 +17,7 @@ exit 0
 fi
 mkdir /var/lib/premium-script;
 echo "IP=" >> /var/lib/premium-script/ipvps.conf
-wget https://raw.githubusercontent.com/santzx45/scmzx/main/cf.sh && chmod +x cf.sh && ./cf.sh
+wget https://raw.githubusercontent.com/tegarrevoer/vps/main/cf.sh && chmod +x cf.sh && ./cf.sh
 #install ssh ovpn
 wget https://raw.githubusercontent.com/santzx45/scmzx/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
 wget https://raw.githubusercontent.com/santzx45/scmzx/main/sstp.sh && chmod +x sstp.sh && screen -S sstp ./sstp.sh
